f7e59b109fac29ca24b0972b90336f3d1b6ef122
esakiryota <esaki1217@gmail.com>
2023-02-25 19:36:28 +0900

    second commit

--- src/connection.js
+++ src/connection.js
@@ -1,6 +1,6 @@
 
-// const api_url = "http://54.238.255.127/api/v.1.0/";
-const api_url = "http://localhost:8080/api/v.1.0/"
+const api_url = "http://54.238.255.127/api/v.1.0/";
+// const api_url = "http://localhost:8080/api/v.1.0/"
 
 // 小説単体系
 //========================================================
