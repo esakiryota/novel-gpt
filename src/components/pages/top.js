1b48145633cb65fcd60697ea08707d20dc5a4309
esakiryota <esaki1217@gmail.com>
2023-02-28 19:57:36 +0900

    resume design

--- src/components/pages/top.js
+++ src/components/pages/top.js
@@ -43,7 +43,7 @@ function Top() {
     }, [])
 
     const TopPageView = styled(Paper)(({ theme }) => ({
-        backgroundImage: `url(${levonia})`,
+        backgroundImage: `url(${process.env.PUBLIC_URL + '/top.jpeg'})`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         margin: -30,
